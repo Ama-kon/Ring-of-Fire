ebafd2e3d558574575f2c26bc858a6247122521a
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-12 00:51:24 +0200

    logic to start a new game & deal cards

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+
+  pickCardAnimation: boolean = false;
+  game!: Game;
+  currentCard: string | undefined = '';
+
+constructor() {}
+
+ngOnInit(): void {
+  this.newGame();
+}
+
+newGame(){
+this.game = new Game();
+console.log(this.game);
+}
+
+  takeCard(){
+    if (!this.pickCardAnimation) {
+      
+      if (this.game.stack.length > 0) {
+        this.currentCard = this.game.stack.pop();
+        this.pickCardAnimation = true;
+       
+        console.log(this.currentCard);
+      } else {
+        console.error("No more cards in the stack");
+    }
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard!);
+      this.pickCardAnimation = false;
+    }, 1000);
+
+    }
+
+}
+}
