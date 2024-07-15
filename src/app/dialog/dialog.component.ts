5c4988760bc35d1b02e5f8e8a6ef7d7c8af66a48
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-07-16 01:11:28 +0200

    formatting only

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject  } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,74 +4,74 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {
-  MatDialog
-} from '@angular/material/dialog';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { DialogOverviewExampleDialog } from '../dialog/dialog.component';
 import { GameInfoComponent } from '../game-info/game-info.component';
-
-
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, GameInfoComponent , MatDialogModule ,MatButtonModule, MatIconModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    PlayerComponent,
+    GameInfoComponent,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-
-export class GameComponent{
-
+export class GameComponent {
   pickCardAnimation: boolean = false;
   game!: Game;
   currentCard: string | undefined = '';
   name!: string;
 
+  constructor(public dialog: MatDialog) {}
 
-constructor(public dialog: MatDialog) {}
+  ngOnInit(): void {
+    this.newGame();
+  }
 
-ngOnInit(): void {
-  this.newGame();
-}
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
-newGame(){
-this.game = new Game();
-console.log(this.game);
-}
-
-  takeCard(){
+  takeCard() {
     if (!this.pickCardAnimation) {
       if (this.game.stack.length > 0) {
         this.currentCard = this.game.stack.pop();
         this.pickCardAnimation = true;
         this.game.currentPlayer++;
-        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+        this.game.currentPlayer =
+          this.game.currentPlayer % this.game.players.length;
       } else {
-        console.error("No more cards in the stack");
+        console.error('No more cards in the stack');
+      }
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard!);
+        this.pickCardAnimation = false;
+      }, 1000);
     }
-    setTimeout(() => {
-      this.game.playedCards.push(this.currentCard!);
-      this.pickCardAnimation = false;
-    }, 1000);
+  }
 
-    }
-}
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { name: ''},
+    });
 
-openDialog(): void {
-  const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-    data: {name: this.name},
-  });
-
-  dialogRef.afterClosed().subscribe((result:string) => {
-    console.log('The dialog was closed');
-    this.name = result;
-    this.game.players.push(result);
-    console.log(this.game.players);
-  });
-}
-
-
-
+    dialogRef.afterClosed().subscribe((result: string) => {
+      if (result && result.length > 0) {
+        //check if result überhaupt vorhanden und dann, ob größer 0
+        this.name = result;
+        this.game.players.push(result);
+      }
+    });
+  
+  }
 }
